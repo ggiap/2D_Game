7c065ee2639327a89ee91238336311d6d0179dc0
ggiap <george.giapotzis@gmail.com>
2020-07-02 17:21:11 +0300

    Replaced sf::Vector2f with sf::Transformable object

--- ECS/Components/C_Transform.hpp
+++ ECS/Components/C_Transform.hpp
@@ -1,10 +1,8 @@
 #pragma once
 
-#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/Transformable.hpp>
 
 struct Transform
 {
-    sf::Vector2f position;
-    sf::Vector2f scale;
-    sf::Vector2f rotation;
+    sf::Transformable transform;
 };
